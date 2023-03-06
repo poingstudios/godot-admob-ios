217f307ecec4ce5c390fdb2c146df6ab1485703a
Gustavo Maciel <gustamaciel2012@hotmail.com>
2023-03-06 09:31:19 -0300

    fix: `generate_static_library.sh` on 3.x Godot

--- scripts/generate_static_library.sh
+++ scripts/generate_static_library.sh
@@ -12,7 +12,16 @@ scons target=$2 arch=x86_64 simulator=yes plugin=$1 version=$3
 
 # Creating a fat libraries for device and simulator
 # lib<plugin>.<arch>-<simulator|ios>.<release|debug|release_debug>.a
-lipo -create "./bin/lib$1.x86_64-simulator.$2.a" \
-    "./bin/lib$1.armv7-ios.$2.a" \
-    "./bin/lib$1.arm64-ios.$2.a" \
-    -output "./bin/$1.$2.a"
+
+if [[ "$3" == "3.x" ]];
+then
+    lipo -create "./bin/lib$1.x86_64-simulator.$2.a" \
+        "./bin/lib$1.armv7-iphone.$2.a" \
+        "./bin/lib$1.arm64-iphone.$2.a" \
+        -output "./bin/$1.$2.a"
+else
+    lipo -create "./bin/lib$1.x86_64-simulator.$2.a" \
+        "./bin/lib$1.armv7-ios.$2.a" \
+        "./bin/lib$1.arm64-ios.$2.a" \
+        -output "./bin/$1.$2.a"
+fi    

8d1127dd014ecd5ce24ad7a4259b188bffa98983
Gustavo Maciel <gustamaciel2012@hotmail.com>
2023-08-21 00:06:10 -0300

    ci: create scripts/download_godot.sh #45
    Usage:
    
    ```bash
    chmod +x ./scripts/download_godot.sh
    ./scripts/download_godot.sh 4.1.1
    ```

---
+++ scripts/download_godot.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+  echo "Error: Please provide the Godot version as an argument."
+  exit 1
+fi
+
+CURRENT_GODOT_VERSION="$1"
+
+GODOT_FOLDER="godot-${CURRENT_GODOT_VERSION}-stable"
+DOWNLOAD_FILE="${GODOT_FOLDER}.tar.xz"
+
+FULL_PATHNAME_DOWNLOAD_GODOT_EXTRACTED_HEADERS="https://github.com/godotengine/godot/releases/download/${CURRENT_GODOT_VERSION}-stable/${DOWNLOAD_FILE}"
+
+curl -LO "$FULL_PATHNAME_DOWNLOAD_GODOT_EXTRACTED_HEADERS"
+tar -xf "$DOWNLOAD_FILE"
+
+if [ -d "godot" ]; then
+  echo "Deleting existing 'godot' folder..."
+  rm -rf "godot"
+fi
+
+mv "$GODOT_FOLDER" godot

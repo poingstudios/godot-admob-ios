7dfaa3f26f8ee6fc120c7fd3fae1ad9171ba99f4
Gustavo Maciel <gustamaciel2012@hotmail.com>
2023-08-21 00:22:30 -0300

    ci: create clean_build.sh #45

---
+++ scripts/clean_build.sh
@@ -0,0 +1,13 @@
+echo "Executing Clean Build"
+
+if [ $# -eq 0 ]; then
+  echo "Error: Please provide the Godot version as an argument."
+  exit 1
+fi
+
+CURRENT_GODOT_VERSION="$1"
+
+./scripts/download_godot.sh ${CURRENT_GODOT_VERSION}
+./scripts/generate_headers.sh || true
+./scripts/release_static_library.sh
+./scripts/create_zip.sh ${CURRENT_GODOT_VERSION}
